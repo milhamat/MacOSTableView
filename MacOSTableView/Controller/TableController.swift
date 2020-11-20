d37b21d02a8249ad3fd11dece37572fcc34c2485
MilhamAt <M477ashiddiq@gmail.com>
2020-11-20 21:42:27 +0700

    add(tableView): first implementing tableView

--- MacOSTableView/Controller/TableController.swift
+++ MacOSTableView/Controller/TableController.swift
@@ -10,14 +10,32 @@ import UIKit
 class TableController: UIViewController {
     
     private var tableView: TableView!
+    
+    let listNumber = ["No.","1","2","3","4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView = TableView(frame: self.view.frame)
         self.view = self.tableView
+        
+        self.tableView.firsTableView.delegate = self
+        self.tableView.firsTableView.dataSource = self
+        
     }
-
-
 }
 
+extension TableController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listNumber.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = listNumber[indexPath.row]
+        return cell
+    }
+    
+    
+}
--- MacOSTableView/View/TableView.swift
+++ MacOSTableView/View/TableView.swift
@@ -10,6 +10,12 @@ import SnapKit
 
 class TableView: UIView {
     
+    let firsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -20,6 +26,12 @@ class TableView: UIView {
     }
     
     private func setupView() {
-        self.backgroundColor = .lightGray
+        self.backgroundColor = .white
+        
+        self.addSubview(firsTableView)
+        
+        self.firsTableView.snp.makeConstraints { (make) in
+            make.top.left.right.bottom.centerX.equalTo(self.safeAreaLayoutGuide)
+        }
     }
 }
